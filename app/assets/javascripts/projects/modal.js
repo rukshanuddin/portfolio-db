82dc4ab8e47cd11423db8087c5fb51aafa9b7488
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-03-19 16:51:13 -0400

    modal

---
+++ app/assets/javascripts/projects/modal.js
@@ -0,0 +1,14 @@
+$(document).on('turbolinks:load', function () {
+    // when a post is clicked, show its full content in a modal window
+    $("body").on("click", ".single-project-card, .single-project-list", function () {
+        var posted_by = $(this).find('.project-content .posted-by').html();
+        var project_heading = $(this).find('.project-content h3').html();
+        var project_content = $(this).find('.project-content p').html();
+        var interested = $(this).find('.project-content .interested').attr('href');
+        $('.modal-header .posted-by').text(posted_by);
+        $('.loaded-data h3').text(project_heading);
+        $('.loaded-data p').text(project_content);
+        $('.loaded-data .interested a').attr('href', interested);
+        $('.myModal').modal('show');
+    });
+});
