fc9aee638eb4a9a75fd3f5a8be85c40d4ec1c471
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-03-19 18:24:47 -0400

    search

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,20 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+  def get_projects
+  branch = params[:action]
+  search = params[:search]
+  flatiron_module = params[:flatiron_module]
+
+  if flatiron_module.blank? && search.blank?
+    projects = Project.by_branch(branch).all
+  elsif flatiron_module.blank? && search.present?
+    projects = Project.by_branch(branch).search(search)
+  elsif flatiron_module.present? && search.blank?
+    projects = Project.by_flatiron_module(branch, flatiron_module)
+  elsif flatiron_module.present? && search.present?
+    projects = Project.by_flatiron_module(branch, flatiron_module).search(search)
+  else
+  end
+end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,19 @@
 class Project < ApplicationRecord
+
   belongs_to :user
   belongs_to :flatiron_module
+
+  default_scope -> { includes(:user).order(created_at: :desc) }
+
+  scope :by_flatiron_module, -> (branch, flatiron_module_name) do 
+    joins(:flatiron_module).where(flatiron_modules: {name: flatiron_module_name, branch: branch}) 
+  end
+
+  scope :by_branch, -> (branch) do
+    joins(:flatiron_module).where(flatiron_modules: {branch: branch}) 
+  end
+
+  scope :search, -> (search) do
+    where("title ILIKE lower(?) OR content ILIKE lower(?)", "%#{search}%", "%#{search}%")
+  end
 end
