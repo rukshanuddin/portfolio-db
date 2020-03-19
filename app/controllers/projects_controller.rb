733b7787cef7fa846e79ad5971e92213d3ad87d0
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-03-19 19:40:23 -0400

    new view for project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,24 +1,32 @@
 class ProjectsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
+  
   def index; end
 
   def show
     @project = Project.find(params[:id])
   end
-  def get_projects
-    branch = params[:action]
-    search = params[:search]
-    flatiron_module = params[:flatiron_module]
 
-    if flatiron_module.blank? && search.blank?
-        projects = Project.by_branch(branch).all
-      elsif flatiron_module.blank? && search.present?
-        projects = Project.by_branch(branch).search(search)
-      elsif flatiron_module.present? && search.blank?
-        projects = Project.by_flatiron_module(branch, flatiron_module)
-      elsif flatiron_module.present? && search.present?
-        projects = Project.by_flatiron_module(branch, flatiron_module).search(search)
-      else
+  def new
+    @branch = params[:branch]
+    @flatiron_modules = FlatironModule.where(branch: @branch)
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save 
+      redirect_to project_path(@project) 
+    else
+      redirect_to root_path
     end
+  end
+  def get_projects
+    ProjectsForBranchService.new({
+      search: params[:search],
+      category: params[:category],
+      branch: params[:action]
+    }).call
   end
   def projects_for_branch
     respond_to do |format|
@@ -26,4 +34,11 @@ class ProjectsController < ApplicationController
       format.js { render partial: 'projects/projects_pagination_page' }
     end
   end
+
+  private 
+  
+    def project_params
+      params.require(:project).permit(:description, :title, :flatiron_module_id)
+                              .merge(user_id: current_user.id)
+    end
 end
