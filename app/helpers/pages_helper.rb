c5608640c07d037856dba1195fb46665ff17ab0d
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-03-23 22:16:27 -0400

    pageshelper  module cleanup

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,8 +1,9 @@
+# Helper modules for root page
 module PagesHelper
-      def login_required_links_partial_path
-    if user_signed_in? 
-      'pages/index/side_menu/login_required_links' 
-    else 
+  def login_required_links_partial_path
+    if user_signed_in?
+      'pages/index/side_menu/login_required_links'
+    else
       'shared/empty_partial'
     end
   end
