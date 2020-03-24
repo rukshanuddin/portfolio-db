156a0b0f910c90c24d39318b972c5c6d604c7807
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-03-23 22:15:21 -0400

    nav helper module cleanup

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,5 +1,5 @@
+# Helper modules for Navbar
 module NavigationHelper
-
   def collapsible_links_partial_path
     if user_signed_in?
       'layouts/navigation/collapsible_elements/signed_in_links'
@@ -7,16 +7,11 @@ module NavigationHelper
       'layouts/navigation/collapsible_elements/non_signed_in_links'
     end
   end
-  
+
   def nav_header_content_partials
     partials = []
-  if params[:controller] == 'messengers' 
-    partials << 'layouts/navigation/header/messenger_header'
-  else # controller is not messengers  
     partials << 'layouts/navigation/header/toggle_button'
     partials << 'layouts/navigation/header/home_button'
-    partials << 'layouts/navigation/header/dropdowns' if user_signed_in?
-  end
     partials
   end
 end
