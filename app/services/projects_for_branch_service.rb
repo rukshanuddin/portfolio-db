a677efcfe6553c23284d042451410d8980111076
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-03-25 13:30:58 -0400

    Cleanup project_for_branch_service.rb

--- app/services/projects_for_branch_service.rb
+++ app/services/projects_for_branch_service.rb
@@ -8,14 +8,8 @@ class ProjectsForBranchService
   def call
     if @flatiron_module.blank? && @search.blank?
       projects = Project.by_branch(@branch).all
-    elsif @flatiron_module.blank? && @search.present?
+    else @flatiron_module.blank? && @search.present?
       projects = Project.by_branch(@branch).search(@search)
-    elsif @flatiron_module.present? && @search.blank?
-      projects = Project.by_flatiron_module(@branch, @flatiron_module)
-    elsif @flatiron_module.present? && @search.present?
-      projects = Project.by_flatiron_module(@branch, @flatiron_module).search(@search)
-    else
     end
   end
-
 end
